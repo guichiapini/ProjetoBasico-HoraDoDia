c9c63368003516d6b3df5b76ddc12d32b81fd577
Guilherme Chiapini <58304587+guichiapini@users.noreply.github.com>
2023-01-28 17:04:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function carregar () {
     var img = window.document.getElementById("imagem");
     var data = new Date();
     var hora = data.getHours();
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var minutos = data.getMinutes();
+    msg.innerHTML = `Agora são ${hora}${minutos} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
         img.src= 'fotomanha.png';
